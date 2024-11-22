5011a214045aa5be211c805362bf68662caaddc5
vital <vital@VITALIY>
2024-11-22 17:17:15 +0200

    Add project files.

---
+++ learn/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Program
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Title = "Learn C#";
+			Console.WriteLine("Hello, world");
+			Console.ReadKey();
+		}
+	}
+}
