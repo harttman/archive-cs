92f8dfc2f1c00b840e3aa8570ea002870e0dfc3c
harttman <dakiv.work.vitaliy@gmail.com>
2024-11-22 18:56:13 +0200

    New logic for output

--- learn/Program.cs
+++ learn/Program.cs
@@ -9,11 +9,17 @@ namespace Program
 			Console.OutputEncoding = System.Text.Encoding.UTF8;
 			Console.InputEncoding = System.Text.Encoding.UTF8;
 
-			Console.WriteLine("\t\t\t\tЧислові Типи\n");
-			Console.WriteLine("Тип\tByte\t|\t.NET\tBYTE\t|\tРозір = 1\t0\t|\tMAX = 255");
-			Console.WriteLine("Тип\tsByte\t|\t.NET\tBYTE\t|\tРозмір = 1\t-128\t|\tMAX = 127");
-			Console.WriteLine("Тип\tshort\t|\t.NET\tInt16\t|\tРозмір = 2\t-3276\t|\tMAX = 37267");
-			Console.WriteLine("Тип\tushort\t|\t.NET\tUInt16\t|\tРозмір = 2\t0\t|\tMAX = 65535");
-		}
+			string msg = "Тип {0,8} |\t.NET {1,8} |\tРозмір = {2} \t| MIN = {3,30}\t| MAX = {4}";
+
+			Console.WriteLine("\n\t\t\t\t\tЧислові типи даних:\n");
+			Console.WriteLine(string.Format(msg, "byte", typeof(byte).Name, sizeof(byte), byte.MinValue, byte.MaxValue));
+            Console.WriteLine(string.Format(msg, "sbyte", typeof(sbyte).Name, sizeof(sbyte), sbyte.MinValue, sbyte.MaxValue));
+            Console.WriteLine(string.Format(msg, "short", typeof(short).Name, sizeof(short), short.MinValue, short.MaxValue));
+            Console.WriteLine(string.Format(msg, "ushort", typeof(ushort).Name, sizeof(ushort), ushort.MinValue, ushort.MaxValue));
+            Console.WriteLine(string.Format(msg, "int", typeof(int).Name, sizeof(int), int.MinValue, int.MaxValue));
+            Console.WriteLine(string.Format(msg, "uint", typeof(uint).Name, sizeof(uint), uint.MinValue, uint.MaxValue));
+            Console.WriteLine(string.Format(msg, "long", typeof(long).Name, sizeof(long), long.MinValue, long.MaxValue));
+            Console.WriteLine(string.Format(msg, "ulong", typeof(ulong).Name, sizeof(ulong), ulong.MinValue, ulong.MaxValue));
+        }
 	}
 }
